5864b80ed8440caa3374ac3015166c27eb54f913
unknown <namelost@protonmail.com>
2019-02-25 15:52:00 +0800

    ex5

---
+++ ex5 More Variables and Printing/ex5.py
@@ -0,0 +1,18 @@
+myName = 'Zed Shaw'
+myAge = 35 #not a lie
+myHeight = 74 #inches
+myWeight = 180 #lbs
+myEyes = 'blue'
+myTeeth = 'White'
+myHair = 'Brown'
+
+print(f'Let\'s talk about {myName}')
+print(f'He\'s {myHeight} inches tall')
+print(f'He\'s {myWeight} pounds heavy')
+print('Actually that\'s not too heavy')
+print(f'He\'s got {myEyes} eyes and {myHair} hair.')
+print(f'His teeth are usually {myTeeth} depending on the coffee.')
+
+# this line is tricky, try to get it exactly right
+total = myAge + myHeight + myWeight
+print(f'If I add {myAge}, {myHeight}, and {myWeight} I get {total}')
