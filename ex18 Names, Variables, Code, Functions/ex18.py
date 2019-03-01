670a26e92d821ca27804ee79a8cc5039aeb3c5d9
unknown <namelost@protonmail.com>
2019-03-01 22:05:14 +0800

    ex18 (v1)

--- ex18 Names, Variables, Code, Functions/ex18.py
+++ ex18 Names, Variables, Code, Functions/ex18.py
@@ -1,6 +1,5 @@
 def print_two(*args):
-  arg1, arg2 = args
-  print(f"arg1: {arg1}, arg2: {arg2}")
+  print(f"arg1: {args[0]}, arg2: {args[1]}")
 
 def print_two_again(arg1, arg2):
   print(f"arg1: {arg1}, arg2: {arg2}")
