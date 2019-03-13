56f4f9993ada04ad059cfaaf7437203c27da4561
unknown <namelost@protonmail.com>
2019-03-13 11:45:56 +0800

    fixed problem with errors when calling wrong quest name

---
+++ ex36 Desiging and Debugging/ex36_v1_spaceRangers.py
@@ -0,0 +1,52 @@
+from quest_read import pick_up_quest
+
+
+def get_quest_elements(raw_data):
+  texts = []
+  steps = []
+  inventory = []
+
+  for line in raw_data:
+    texts.append(line[0])  
+    steps.append(line[1])
+    inventory.append(line[2])
+  
+  return [texts, steps, inventory]
+
+def print_line(arr, index):
+  if arr == all_inventory:
+    print("У вас есть: ")
+  print(arr[index].format(user_name))
+
+def make_decision(index):
+  print_line(all_texts, index)
+  user_choice = input("> ")
+
+  try:
+    user_choice = int(user_choice)
+
+    # if user_choice
+
+  except:
+    if user_choice == 'exit':
+      return
+    
+    print("Введите правильный вариант или exit")
+
+    make_decision(index)
+
+
+def start():
+  global all_texts, all_steps, all_inventory, user_name
+  all_quest = pick_up_quest()
+
+  user_name = input("Введите ваше имя: ")
+
+  all_quest_lines = get_quest_elements(all_quest)
+  all_texts = all_quest_lines[0]
+  all_steps = all_quest_lines[1]
+  all_inventory = all_quest_lines[2]
+  make_decision(0)
+
+
+start()
--- ex36 Desiging and Debugging/quest_read.py
+++ ex36 Desiging and Debugging/quest_read.py
@@ -1,26 +1,17 @@
-
 def pick_up_quest():
+  all_lines_formatted = []
   available_quests = ['menzols']
   print("Какой квест вы хотите пройти?")
   print("Доступны квесты: ", available_quests)
   chosen_quest = input("> ")
 
-  is_available = check_quest_availability(available_quests, chosen_quest)
-
-  if is_available:
+  if available_quests.__contains__(chosen_quest):
     all_quest_lines = read_all_quest(chosen_quest)
-    return format_all_lines(all_quest_lines)
-
+    all_lines_formatted = format_all_lines(all_quest_lines)
+    return all_lines_formatted
   else:
-    print("Такого квеста пока нет!")
-    pick_up_quest()
-
-
-
-def check_quest_availability(all_quests, quest_name):
-  if all_quests.__contains__(quest_name):
-    return True
-  return False
+    return pick_up_quest()
+  
 
 def read_all_quest(quest_name):
   txt = open(f"{quest_name}.txt", encoding='utf-8')
@@ -32,29 +23,26 @@ def read_all_quest(quest_name):
 def format_all_lines(all_lines):
   formatted_lines = []
   for line in all_lines:
-    formatted_lines.append(format_line(line))
+    formatted_lines.append(get_formated_line(line))
 
   return formatted_lines
 
-def format_line(text_line):
-  string_and_steps = get_formated_arr(text_line, '*next_step*')
-  string = string_and_steps[0]
-  next_steps = string_and_steps[1]
+def get_formated_line(text_line):
+  next_steps = text_line.split('*next_step*')
+  string = ''.join(next_steps[0])
+  next_steps.pop(0)
 
-  string_and_inventory = get_formated_arr(string, '*inventory*')
-  string = string_and_inventory[0]
-  inventory = string_and_inventory[1]
+  if text_line.__contains__('*inventory*'):
+    inventory = next_steps.pop()
+    inventory = inventory.replace('*inventory*', '')
+  else:
+    inventory = ''
+    next_steps.pop()
   
+  next_steps = next_steps[0].split(',')
+
   string = string.replace('*-*', '\n')
   string = string.replace('*next_step*', '')
   string = string.replace('*inventory*', '')
 
   return [string, next_steps, inventory]
-
-def get_formated_arr(text_line, form):
-  formated_string = text_line.split(form)
-  string = ''.join(formated_string[0])
-  formated_string.pop(0)
-
-  return [string, formated_string]
-  
