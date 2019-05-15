1f9f3c6cf61b6ea2cab96236e3d59027bf8b6d6a
alexgrand <namelost@protonmail.com>
2019-05-15 12:22:40 +0800

    added run , in

--- ex49 Making Sentences/ex48/lexicon.py
+++ ex49 Making Sentences/ex48/lexicon.py
@@ -4,10 +4,10 @@ class Lexicon(object):
             'north': 'direction', 'south': 'direction', 'east': 'direction',
             'west': 'direction', 'down': 'direction', 'up': 'direction',
             'left': 'direction', 'right': 'direction', 'back': 'direction',
-            'go': 'verb', 'stop': 'verb', 'kill': 'verb', 'eat': 'verb',
-            'the': 'stop', 'in': 'stop', 'of': 'stop', 'from': 'stop',
-            'and': 'stop', 'at': 'stop', 'it': 'stop',
-            'door': 'noun', 'bear': 'noun',
+            'go': 'verb', 'run': 'verb', 'stop': 'verb', 'kill': 'verb',
+            'eat': 'verb', 'to': 'stop', 'the': 'stop', 'in': 'stop',
+            'of': 'stop', 'from': 'stop', 'and': 'stop', 'at': 'stop',
+            'it': 'stop', 'door': 'noun', 'bear': 'noun',
             'princess': 'noun', 'cabinet': 'noun'
         }
 
