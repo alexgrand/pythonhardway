655ab74ba4e747040d90be9c69791a29f8c47136
Alex <example@mail.com>
2019-05-20 19:53:33 +0800

    flask now can work without __name__

--- ex50 Your First Website/gothonweb/app.py
+++ ex50 Your First Website/gothonweb/app.py
@@ -1,16 +1,14 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    greeting = "World"
-    return f'Hello, {greeting}!'
+    greeting = "Hello World"
+    return render_template("index.html", greeting=greeting)
 
 
 @app.route('/nihao')
 def nihao_world():
     return 'Nihao, World'
-
-if __name__ == "__main__":
-    app.run()
