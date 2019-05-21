c2467d7bfe7abf3162404b586c29120ef3e11f9b
Alex <example@mail.com>
2019-05-21 12:30:57 +0800

    testing hello.py

--- ex51 Getting INput from a Browser/gothonweb/hello.py
+++ ex51 Getting INput from a Browser/gothonweb/hello.py
@@ -17,12 +17,5 @@ def hello():
     else:
         return render_template("hello_form.html")
 
-
-@app.route("/")
-def index():
-    greeting = "Hello, World"
-
-    return render_template("index.html", greeting=greeting)
-
 if __name__ == "__main__":
     app.run()
